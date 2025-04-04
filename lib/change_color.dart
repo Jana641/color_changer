266a4dd7f7eef381ca07cee8249344046177c023
Jana641 <101220641@mu.edu.lb>
2025-04-05 00:20:50 +0300

    added the third commit

--- lib/change_color.dart
+++ lib/change_color.dart
@@ -21,34 +21,63 @@ class _ChangeColorState extends State<ChangeColor> {
    Random().nextInt(256));
  });
   }
+
+   Alignment initialDirection = Alignment.topCenter;
+  int directionIndex = 0;
+
+  void changeDirection() {
+    setState(() {
+  List<Alignment> directions = [
+    Alignment.topCenter,       
+    Alignment.centerLeft,      
+    Alignment.topLeft,       
+    Alignment.topRight,   
+  ];
+
+      directionIndex = (directionIndex + 1) % directions.length;
+      initialDirection = directions[directionIndex];
+    });
+  }
   @override
   Widget build(BuildContext context) {
    return  Center(
      child: Container(
       
             decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [initialColor, Colors.white],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
+             gradient: LinearGradient(
+              colors: [initialColor, Colors.white],
+              begin: initialDirection,
+              end: Alignment(-initialDirection.x, -initialDirection.y),
               ),
+
+
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Center(
-                  child: ElevatedButton( style: ElevatedButton.styleFrom(side: BorderSide(color: Colors.white) )  , onPressed: ChangeColor, child: Text('Change Color', style: TextStyle(color:Color.fromARGB(255, 112, 106, 106) , fontSize: 15),)),
+                  child: ElevatedButton( style: ElevatedButton.styleFrom(side: BorderSide(color: Colors.white), padding: EdgeInsets.symmetric(horizontal: 35, vertical: 14), )  , onPressed: ChangeColor, child: Text('Change Color', style: TextStyle(color:Color.fromARGB(255, 112, 106, 106) , fontSize: 15),)),
                   ),
-                  SizedBox(height: 20),
+              SizedBox(height: 20),
               Text(
               ' Red ${initialColor.red}, Green ${initialColor.green},Blue ${initialColor.blue}',
               style: TextStyle(color: Colors.black, fontSize: 20),
+
+              ),
+              SizedBox(height: 20),
+              ElevatedButton( style: ElevatedButton.styleFrom(side: BorderSide(color: Colors.white) )  ,onPressed: changeDirection, child: Text('Change Direction', style: TextStyle(color:Color.fromARGB(255, 112, 106, 106) , fontSize: 15),)),
+               SizedBox(height: 20),
+              Text(
+              ' Direction: ${initialDirection}',
+              style: TextStyle(color: Colors.black, fontSize: 20),
+
               ),
               ],
               
             ),
-              
+         
           ),
+
    );
   }
 
